93e566eab7ecd66a006be05b1c6a1299b3f9bf40
kevin <kevinmbari600@gmail.com>
2024-01-15 14:09:19 +0300

    assignment done

--- server/app.py
+++ server/app.py
@@ -47,6 +47,29 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+        data = request.get_json()
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return '', 204
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
